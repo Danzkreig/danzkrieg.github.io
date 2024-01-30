4fff985a62ce484d1ac117aa8c584baff1ae88b6
Termina <festeringwound@proton.me>
2024-01-30 19:33:34 +0800

    FLL

--- blogsite/blog/src/app/page.js
+++ blogsite/blog/src/app/page.js
@@ -1,3 +1,29 @@
+"use client";
+import { useState, useEffect } from "react";
 export default function Raskas() {
-  return <h1>Hello waghh.</h1>;
+  const [articles, setArticles] = useState([]);
+  const [filteredArticles, setFilteredArticles] = useState(articles);
+
+  const fetchData = async () => {
+    const article = await fetch("https://dev.to/api/articles?per_page=10").then(
+      (res) => res.json().then(setArticles(res)).then(setFilteredArticles(data))
+    );
+  };
+  useEffect(() => {
+    fetchData();
+  });
+
+  const handleSearch = (event) => {
+    const filteredArticles = articles.filter((article) =>
+      article.title.includes(event.target.value)
+    );
+    setFilteredArticles(filteredArticles);
+  };
+  return (
+    <div>
+      <div className="flex w-screen justify-center">
+        <input onChange={handleSearch} type="text" className="border-2"></input>
+      </div>
+    </div>
+  );
 }
