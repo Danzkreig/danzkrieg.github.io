264c2e8dcff0d5ad84cf62acea142fa97bc2e739
Termina <festeringwound@proton.me>
2024-02-20 16:09:54 +0800

    asdf

--- blogsite/blog/src/app/single/page.js
+++ blogsite/blog/src/app/single/page.js
@@ -7,6 +7,7 @@ export default function single() {
   const searchParams = useSearchParams();
   const search = searchParams.get("id");
   const [article, SetArticle] = useState([]);
+
   async function getBata() {
     const geschutz = await fetch("https://dev.to/api/articles/" + search)
       .then((response) => response.json())
@@ -15,27 +16,30 @@ export default function single() {
   useEffect(() => {
     getBata();
   }, [SetArticle]);
-
-  return (
-    <div>
-      <div className="flex flex-col justify-center items-center relative w-5/6 mr-auto ml-auto text-center">
-        <h1 className="text-3xl">{article.title}</h1>
-        <div className="flex flex-row mt-3">
-          <Image src="/favicon.ico" width={25} height={20} />
-          <h1 className="mr-10">John Doe</h1>
-          <h3>{article.created_at}</h3>
+  if (search !== "") {
+    return (
+      <div>
+        <div className="flex flex-col justify-center items-center relative w-5/6 mr-auto ml-auto text-center">
+          <h1 className="text-3xl">{article.title}</h1>
+          <div className="flex flex-row mt-3">
+            <Image src="/favicon.ico" width={25} height={20} />
+            <h1 className="mr-10">John Doe</h1>
+            <h3>{article.created_at}</h3>
+          </div>
+          <Image
+            src={article.social_image}
+            width={1200}
+            height={300}
+            className="mt-10 mb-10"
+          />
+          <h4
+            className="mb-10"
+            dangerouslySetInnerHTML={{ __html: article.body_html }}
+          ></h4>
         </div>
-        <Image
-          src={article.social_image}
-          width={1200}
-          height={300}
-          className="mt-10 mb-10"
-        />
-        <h4
-          className="mb-10"
-          dangerouslySetInnerHTML={{ __html: article.body_html }}
-        ></h4>
       </div>
-    </div>
-  );
+    );
+  } else {
+    <div>404</div>;
+  }
 }
